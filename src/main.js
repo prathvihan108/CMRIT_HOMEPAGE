4e860eb2f031aaad3ce77b762598c80d9a46ba3b
Prathvihan1008 <prathvihan1008@gmail.com>
2025-01-19 16:16:32 +0530

    initilised the new repo as the sub module

---
+++ src/main.js
@@ -0,0 +1,23 @@
+const content = document.querySelector(".content-class");
+
+// // Dynamically load the initial page
+// fetch("/src/sections/initial_page/initial.html") // Adjusted to match the Vite structure
+// 	.then((response) => response.text())
+// 	.then((data) => {
+// 		content.innerHTML = data;
+
+// 		// Manually add the CSS link to the document head
+// 		const link = document.createElement("link");
+// 		link.rel = "stylesheet";
+// 		link.href = "/src/sections/initial_page/initial.css"; // Adjusted path for CSS
+// 		document.head.appendChild(link);
+
+// 		// Load or attach the JS file
+// 		const script = document.createElement("script");
+// 		script.src = "/src/sections/initial_page/initial.js"; // Adjusted path for JS
+// 		document.body.appendChild(script);
+// 	})
+// 	.catch((error) => console.error("Error:", error));
+
+// Hover effect and the link naviagation for the navbar
+window.location.href = "/src/sections/initial_page/initial.html";
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+// vite.config.js
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
+export default {
+	server: {
+		host: true, // Allows access via your local IP address
+		port: 3000, // Default port (change if needed)
+		hmr: true,
+	},
+	plugins: [
+		handlebars({
+			partialDirectory: resolve(__dirname, "src/partials"),
+			// templateFileExtension: "html",
+		}),
+	],
+};
